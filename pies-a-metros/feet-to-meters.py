168135af427f8de40659d5f18930bb2745e5be28
MarinaArrieta <arrietamarina12@gmail.com>
2025-05-15 12:51:45 -0300

    cambio del bg

--- pies-a-metros/feet-to-meters.py
+++ pies-a-metros/feet-to-meters.py
@@ -11,6 +11,13 @@ def calculate(*args):
 root = Tk()
 root.title("Pies a Metros")
 
+# Estilo para cambiar el fondo
+style = ttk.Style()
+style.configure("TFrame", background="#FFBE0B")  # Fondo oscuro
+style.configure("TLabel", background="#FFBE0B", foreground="#000814")  # Letras blancas
+style.configure("TButton", background="#FFBE0B")  # Botón oscuro
+style.map("TButton", background=[("active", "#FB5607")])  # Color al pasar el mouse
+
 mainframe = ttk.Frame(root, padding="1 1 5 5")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
@@ -20,13 +27,15 @@ feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
 feet_entry.grid(column=2, row=1, sticky=(W, E))
 
+# feet_entry.configure(background="#1c1c1c", foreground="white", insertbackground="white")
+
 meters = StringVar()
 ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
 ttk.Button(mainframe, text="Calcular", command=calculate).grid(column=3, row=3, sticky=W)
 
 ttk.Label(mainframe, text="Pies").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="es equivalente a").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="es equivalente a: ").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="Metros").grid(column=3, row=2, sticky=W)
 
 for child in mainframe.winfo_children(): 
