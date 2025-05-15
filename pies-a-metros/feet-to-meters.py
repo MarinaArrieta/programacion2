65f92bd62988043bb954eb489b180b33892051db
MarinaArrieta <arrietamarina12@gmail.com>
2025-05-15 12:04:54 -0300

    pies a metros y Omni Theme

---
+++ pies-a-metros/feet-to-meters.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+root = Tk()
+root.title("Pies a Metros")
+
+mainframe = ttk.Frame(root, padding="1 1 5 5")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calcular", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="Pies").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="es equivalente a").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="Metros").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
